17f788bba3b5cc0097aa947d6823d529f77d289b
tMinamiii <takahiro.minami@every.tv>
2022-10-05 11:00:34 +0900

    rebuild

--- main.go
+++ main.go
@@ -4,12 +4,14 @@ import (
 	"archive/tar"
 	"compress/gzip"
 	"encoding/json"
+	"flag"
 	"fmt"
 	"io"
 	"io/fs"
 	"log"
 	"net/http"
 	"os"
+	"os/exec"
 	"path/filepath"
 	"runtime"
 	"strings"
@@ -17,34 +19,47 @@ import (
 	"github.com/hashicorp/go-version"
 )
 
+const DefaultBase = "/usr/local/go"
+const Repository = "github.com/tMinamiii/go-update"
+
 type GoDownloadCandidate struct {
 	Version string `json:"version"`
 }
 
 type GoDownloadCandidates []GoDownloadCandidate
 
-func fetchLatestVersion() (string, error) {
+func rebuildGoUpdate() error {
+	gobin := DefaultBase + "/bin/go"
+	repos := Repository + "@latest"
+	_, err := exec.Command(gobin, "install", repos).Output()
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func fetchLatestVersion() (*string, error) {
 	resp, err := http.Get("https://go.dev/dl/?mode=json")
 	if err != nil {
-		return "", nil
+		return nil, nil
 	}
 	defer resp.Body.Close()
 
 	candidates := GoDownloadCandidates{}
 	b, err := io.ReadAll(resp.Body)
 	if err != nil {
-		return "", nil
+		return nil, nil
 	}
 
 	err = json.Unmarshal(b, &candidates)
 	if err != nil {
-		return "", nil
+		return nil, nil
 	}
 	if len(candidates) == 0 {
-		return "", fmt.Errorf("no candidates")
+		return nil, fmt.Errorf("no candidates")
 	}
 
-	return candidates[0].Version, nil
+	return &candidates[0].Version, nil
 }
 
 func copyFileTgz(base string, from *tar.Reader, header *tar.Header) error {
@@ -99,48 +114,30 @@ func packageName(version string) string {
 	return pkgName
 }
 
-func IsLatest(latest, current string) (bool, error) {
+func checkVersion(target, current string) {
 	currentVersion, err := version.NewVersion(strings.Replace(current, "go", "", 1))
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
 
-	latestVersion, err := version.NewVersion(strings.Replace(latest, "go", "", 1))
+	targetVersion, err := version.NewVersion(strings.Replace(target, "go", "", 1))
 	if err != nil {
 		log.Fatal(err)
 		os.Exit(1)
 	}
 
-	return latestVersion.LessThanOrEqual(currentVersion), nil
+	if targetVersion.LessThanOrEqual(currentVersion) {
+		fmt.Printf("Target version already installed. -- %s\n", current)
+		os.Exit(0)
+	}
 }
 
-func main() {
-	if runtime.GOOS == "windows" {
-		fmt.Println("windows is incompatible")
-		os.Exit(0)
-	}
+func install(target, current string) error {
+	fmt.Printf("Your version is %s and latest version is %s\n", current, target)
+	fmt.Printf("Start the Installation %s\n", target)
 
-	latest, err := fetchLatestVersion()
-	if err != nil {
-		log.Fatal(err)
-		os.Exit(1)
-	}
-
-	isLatest, err := IsLatest(latest, runtime.Version())
-	if err != nil {
-		log.Fatal(err)
-		os.Exit(1)
-	}
-	if isLatest {
-		fmt.Printf("Latest version already installed. -- %s\n", runtime.Version())
-		os.Exit(0)
-	}
-
-	fmt.Printf("Your version is %s and latest version is %s\n", runtime.Version(), latest)
-	fmt.Printf("Start the Installation %s\n", latest)
-
-	url := fmt.Sprintf("https://go.dev/dl/%s", packageName(latest))
+	url := fmt.Sprintf("https://go.dev/dl/%s", packageName(target))
 	resp, err := http.Get(url)
 	if err != nil {
 		log.Fatal(err)
@@ -149,15 +146,46 @@ func main() {
 
 	defer resp.Body.Close()
 
-	const base = "/usr/local/go"
-	err = os.RemoveAll(base)
+	err = os.RemoveAll(DefaultBase)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	err = extractTgz(base, resp)
+	err = extractTgz(DefaultBase, resp)
 	if err != nil && err != io.EOF {
 		log.Fatal(err)
 	}
-	fmt.Printf("latest version %s installed\n", latest)
+	return nil
+}
+
+func main() {
+	target := flag.String("v", "", "version")
+
+	flag.Parse()
+	current := runtime.Version()
+	if runtime.GOOS == "windows" {
+		fmt.Println("windows is incompatible")
+		os.Exit(0)
+	}
+
+	if *target == "" {
+		var err error
+		target, err = fetchLatestVersion()
+		if err != nil {
+			log.Fatal(err)
+			os.Exit(1)
+		}
+
+	}
+	checkVersion(*target, runtime.Version())
+
+	err := install(*target, current)
+	if err != nil {
+		log.Fatal(err)
+		os.Exit(1)
+	}
+
+	rebuildGoUpdate()
+
+	fmt.Printf("latest version %s installed\n", *target)
 }
